269f9723856d585c3a14e919696c17697254ce95
Vishesh Dubey <visheshdubey2016@gmail.com>
2025-01-27 20:54:40 +0530

    Revert "feat: initial NextAuth setup"

--- src/server/lib/email/templates/magic-link.ts
+++
@@ -1,7 +0,0 @@
-export const MagicLinkTemplate = (email: string, link: string) => {
-  return {
-    to: email,
-    subject: "Your Magic Link",
-    html: `<p>Click the link to sign in: ${link}</p>`,
-  };
-};
