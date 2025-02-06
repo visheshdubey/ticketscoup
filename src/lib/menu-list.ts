8a0ef44bfe1ff8a1a6781808cf5216d73ce444bf
Ankitjigs <ankitjiganwal@gmail.com>
2025-02-07 00:48:00 +0530

    feat: required roles added in menulist

--- src/lib/menu-list.ts
+++ src/lib/menu-list.ts
@@ -1,4 +1,5 @@
 import { House, Store, Users, Contact, Bell, Settings } from "lucide-react";
+import { Role } from "@prisma/client";
 
 export function getMenuList(pathname: string) {
   return {
@@ -6,48 +7,94 @@ export function getMenuList(pathname: string) {
     items: [
       {
         title: "Overview",
-        url: "/overview",
+        url: "/dashboard/overview",
         icon: House,
+        requiredRoleAny: [
+          Role.ADMIN,
+          Role.MANAGER,
+          Role.STAFF,
+          Role.DEVELOPER,
+          Role.CLIENT,
+        ],
       },
       {
         title: "Tickets",
-        url: "/tickets",
+        url: "/dashboard/tickets",
         icon: Store,
+        requiredRoleAny: [
+          Role.ADMIN,
+          Role.MANAGER,
+          Role.STAFF,
+          Role.DEVELOPER,
+          Role.CLIENT,
+        ],
       },
       {
         title: "Clients",
-        url: "/clients",
+        url: "/dashboard/clients",
         icon: Users,
+        requiredRoleAny: [Role.ADMIN, Role.MANAGER, Role.STAFF],
       },
       {
         title: "Staff",
-        url: "/staff",
+        url: "/dashboard/staff",
         icon: Contact,
+        requiredRoleAny: [Role.ADMIN, Role.MANAGER],
       },
       {
         title: "Notifications",
         url: "#",
         icon: Bell,
+        requiredRoleAny: [
+          Role.ADMIN,
+          Role.MANAGER,
+          Role.STAFF,
+          Role.DEVELOPER,
+          Role.CLIENT,
+        ],
       },
       {
         title: "Settings",
         icon: Settings,
+        requiredRoleAny: [
+          Role.ADMIN,
+          Role.MANAGER,
+          Role.DEVELOPER,
+          Role.STAFF,
+          Role.CLIENT,
+        ],
         items: [
           {
             title: "Ticket Types",
             url: "#",
+            requiredRoleAny: [Role.ADMIN, Role.MANAGER, Role.DEVELOPER],
           },
           {
             title: "Organization Info",
             url: "#",
+            requiredRoleAny: [Role.ADMIN, Role.MANAGER, Role.DEVELOPER],
           },
           {
             title: "Personal Info",
             url: "#",
+            requiredRoleAny: [
+              Role.ADMIN,
+              Role.MANAGER,
+              Role.STAFF,
+              Role.DEVELOPER,
+              Role.CLIENT,
+            ],
           },
           {
             title: "Help & Support",
             url: "#",
+            requiredRoleAny: [
+              Role.ADMIN,
+              Role.MANAGER,
+              Role.STAFF,
+              Role.DEVELOPER,
+              Role.CLIENT,
+            ],
           },
         ],
       },
