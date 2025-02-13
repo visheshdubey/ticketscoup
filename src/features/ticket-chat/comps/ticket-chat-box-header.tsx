3cc674d34af125a6d183df549c38033bf8d5263d
Ankitjigs <ankitjiganwal@gmail.com>
2025-02-14 02:38:55 +0530

    fix: removed fragment

--- src/features/ticket-chat/comps/ticket-chat-box-header.tsx
+++ src/features/ticket-chat/comps/ticket-chat-box-header.tsx
@@ -11,18 +11,16 @@ export function TicketChatBoxHeader(props: TicketChatBoxHeaderProps) {
     const userInitials = getNameInitials(userName);
 
     return (
-        <>
-            <div className="w-full bg-white border-b px-4 py-0.5">
-                <div className="flex relative items-center gap-4 py-1.5">
-                    <UserAvatar userInitials={userInitials} className="size-9" />
-                    <div className="flex flex-col">
-                        <span className="font-satoshi font-medium text-base text-[#0C0A09]">{userName}</span>
-                        <div className="flex items-center">
-                            <span className="font-medium font-satoshi text-[#A09B96] text-xs">{id}</span>
-                        </div>
+        <div className="w-full bg-white border-b px-4 py-0.5">
+            <div className="flex relative items-center gap-4 py-1.5">
+                <UserAvatar userInitials={userInitials} className="size-9" />
+                <div className="flex flex-col">
+                    <span className="font-satoshi font-medium text-base text-[#0C0A09]">{userName}</span>
+                    <div className="flex items-center">
+                        <span className="font-medium font-satoshi text-[#A09B96] text-xs">{id}</span>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
