21127fba8bc6cdfd94ba0f137f78bcf10832555f
Ankitjigs <ankitjiganwal@gmail.com>
2025-02-14 15:13:06 +0530

    fix: formatting space

--- src/lib/date-time.ts
+++ src/lib/date-time.ts
@@ -2,5 +2,6 @@ import { format, parseISO } from 'date-fns';
 
 export function formatTimestampToLocaleTime(timestamp: string) {
     const localeDate = parseISO(timestamp);
+
     return format(localeDate, 'hh:mm a');
 }
