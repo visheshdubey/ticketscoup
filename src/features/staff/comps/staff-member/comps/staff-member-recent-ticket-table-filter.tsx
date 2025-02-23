0d8174f813d16a1a160b2371c40c18c85c794102
Ankitjigs <ankitjiganwal@gmail.com>
2025-02-23 14:53:35 +0530

    feat: staff member-page added

---
+++ src/features/staff/comps/staff-member/comps/staff-member-recent-ticket-table-filter.tsx
@@ -0,0 +1,14 @@
+import { ListFilterIcon } from 'lucide-react';
+
+type Props = {
+    filtersApplied: boolean;
+};
+
+export function StaffMemberRecentTicketTableFilter({ filtersApplied }: Props) {
+    return (
+        <div className="bg-[#EAECFC] rounded-lg p-2 cursor-pointer relative">
+            <ListFilterIcon size={16} />
+            {filtersApplied && <div className="absolute top-1 right-1 w-[5px] h-[5px] bg-[#0057CC] rounded-full" />}
+        </div>
+    );
+}
